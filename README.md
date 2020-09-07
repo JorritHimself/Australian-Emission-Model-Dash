b1ce33cf57ec08d00df0e2f80649b69401f6cf15
JorritHimself <JorritHimself@users.noreply.github.com>
2020-09-07 18:17:15 +1000

    added better readme and extra slider

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Australian emissions model in Python/Plotly/Dash
 
 
-# How to import
+# How to import in local installation
 Easiest is to use anaconda navigator. 
 Go to 'environments', impot, and select the environment.yml file. 
 
@@ -9,4 +9,16 @@ Alternatively, via conda:
 `conda env create -f environment.yml`
 followed by 
 `activate ausenergydash`
+
+# How to make it work in python anywhere
+Copy the contents of WSIG for python anywhere.txt into the pyhton anywhere, in the web tab
+Make sure you change the run the app with:
+`if __name__ == '__main__':
+    
+	app.run_server()`
+Import the prepped data
+ correctly with full directory:
+`df_final = pd.read_csv('/home/JorritHimself/mysite/db/preppeddata.csv')`
+
+
 
--- dashapp.py
+++ dashapp.py
@@ -40,6 +40,8 @@ app.layout = html.Div([
     dcc.Graph(id='graph-with-sliders'),
     html.H2('Agriculture emissions trend:'),
     dcc.Slider(id='agri-emis-slider', min=-20, max=20, value=0.54, step=0.01, marks={i: '{}'.format(i) for i in range(-20, 20)}),
+    html.H2('Electricity generation emissions trend:'),
+    dcc.Slider(id='elec-emis-slider', min=-20, max=20, value=-1.96, step=0.01, marks={i: '{}'.format(i) for i in range(-20, 20)}),
     html.H2('LULUCF emissions trend:'),
     dcc.Slider(id='lulucf-emis-slider', min=-20, max=20, value=-12.2, step=0.01, marks={i: '{}'.format(i) for i in range(-20, 20)})
 ])
@@ -48,13 +50,16 @@ app.layout = html.Div([
 @app.callback(
     Output('graph-with-sliders', 'figure'),
     [Input('agri-emis-slider', 'value'),
+     Input('elec-emis-slider', 'value'),
      Input('lulucf-emis-slider', 'value')])
-def update_figure(agri_emis_trend, lulucf_emis_trend):
-    ### Emissions output agriculutre: emissions levels at last observation, minus number of years since final observation *annual emission reductions
+def update_figure(agri_emis_trend, elec_emis_trend, lulucf_emis_trend):
+    ### Emissions output agriculutre: emissions levels at last observation, minus number of years since final observation *annual emission reductions. Second line si so they cannot go negative
     df_nat.loc[(df_nat['sector']=='Agriculture') & (df_nat['yrs_since_final_obs']>0),'emissions_MtCo2_output'] = df_nat['emissions_MtCo2_finaly']+agri_emis_trend*df_nat['yrs_since_final_obs']
-    # Agri emissions cannot go negative:
     df_nat.loc[(df_nat['sector']=='Agriculture') & (df_nat['emissions_MtCo2_output']>0), 'emissions_MtCo2_output'] = 0
-    ### Emissions output LULUCF: emissions levels at last observation, minus number of years since final observation *annual emission reductions
+    ### Emissions output electricity: emissions levels at last observation, minus number of years since final observation *annual emission reductions. Second line si so they cannot go negative
+    df_nat.loc[(df_nat['sector']=='Electricity generation') & (df_nat['yrs_since_final_obs']>0),'emissions_MtCo2_output'] = df_nat['emissions_MtCo2_finaly']+elec_emis_trend*df_nat['yrs_since_final_obs']
+    df_nat.loc[(df_nat['sector']=='Electricity generation') & (df_nat['emissions_MtCo2_output']>0), 'emissions_MtCo2_output'] = 0
+        ### Emissions output LULUCF: emissions levels at last observation, minus number of years since final observation *annual emission reductions
     df_nat.loc[(df_nat['sector']=='LULUCF') & (df_nat['yrs_since_final_obs']>0),'emissions_MtCo2_output'] = df_nat['emissions_MtCo2_finaly']+lulucf_emis_trend*df_nat['yrs_since_final_obs']
     
     fig = px.area(df_nat, x="year", y="emissions_MtCo2_output", color="sector")
@@ -63,6 +68,6 @@ def update_figure(agri_emis_trend, lulucf_emis_trend):
     return fig
 
 if __name__ == '__main__':
-    app.run()
+    app.run_server(debug=True)
     
     
